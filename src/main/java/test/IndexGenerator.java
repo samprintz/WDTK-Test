d9e4089da364708dafb4090dea23ed7fb0aa5629
Samuel Printz <sam.printz@web.de>
2016-11-10 15:30:44 +0100

    Single entities can be processed. Extracted methods.

--- src/main/java/test/IndexGenerator.java
+++ src/main/java/test/IndexGenerator.java
@@ -6,9 +6,11 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+import org.wikidata.wdtk.datamodel.interfaces.EntityDocument;
 import org.wikidata.wdtk.datamodel.interfaces.EntityDocumentProcessor;
 import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
 import org.wikidata.wdtk.datamodel.interfaces.MonolingualTextValue;
@@ -18,6 +20,8 @@ import org.wikidata.wdtk.dumpfiles.DumpProcessingController;
 import org.wikidata.wdtk.dumpfiles.EntityTimerProcessor;
 import org.wikidata.wdtk.dumpfiles.MwLocalDumpFile;
 import org.wikidata.wdtk.examples.ExampleHelpers;
+import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher;
+import org.wikidata.wdtk.wikibaseapi.apierrors.MediaWikiApiErrorException;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
@@ -30,7 +34,7 @@ public class IndexGenerator implements EntityDocumentProcessor {
 	/**
 	 * The dump which entites should be processed..
 	 */
-	private final static String DUMP_FILE = "B:/20161107-wikidata_dump/dumpfiles/wikidatawiki/json-20161031/20161031-head-1000.json.gz";
+	private final static String DUMP_FILE = "B:/20161107-wikidata_dump/dumpfiles/wikidatawiki/json-20161031/20161031-head-100.json.gz";
 	// private final static String DUMP_FILE =
 	// "C:/Daten/Eclipse/wdtk-parent/wdtk-examples/dumpfiles/wikidatawiki/json-20161031/20161031.json.gz";
 	// private final static String DUMP_FILE =
@@ -67,7 +71,8 @@ public class IndexGenerator implements EntityDocumentProcessor {
 		jsonGen.setCodec(new ObjectMapper());
 		jsonGen.setPrettyPrinter(new MinimalPrettyPrinter(""));
 
-		// TODO Ist noch kein JSON array, schließende Klammer wird irgendwie nicht geschrieben (s.u.)
+		// TODO Ist noch kein JSON array, schließende Klammer wird irgendwie
+		// nicht geschrieben (s.u.)
 		// jsonGen.writeRaw("[");
 
 		// Select dump file
@@ -98,54 +103,19 @@ public class IndexGenerator implements EntityDocumentProcessor {
 		// TODO
 		// jsonGen.writeRaw("]");
 
+		indexGenerator.processItemDocumentById("Q1726");
+
 		entityTimerProcessor.close();
 
 	}
 
 	public IndexGenerator() {
-
 	}
 
 	public void processItemDocument(ItemDocument itemDocument) {
-		IndexEntity indexEntity = new IndexEntity();
+		IndexEntity indexEntity = retrieveIndexEntity(itemDocument);
+		writeToIndex(indexEntity);
 
-		// ID
-		indexEntity.id = itemDocument.getItemId().getId();
-
-		// Surface forms
-		Set<String> languages = itemDocument.getLabels().keySet();
-		for (String language : languages) {
-			List<String> surfaceForms = new ArrayList<String>();
-			// Label
-			String label = itemDocument.findLabel(language);
-			if (label != null) {
-				surfaceForms.add(label);
-			}
-			// Aliases
-			List<MonolingualTextValue> aliases = itemDocument.getAliases().get(language);
-			if (aliases != null) {
-				for (MonolingualTextValue alias : aliases) {
-					surfaceForms.add(alias.getText());
-				}
-			}
-			indexEntity.surfaceForms.put(language, surfaceForms);
-		}
-
-		// Statistics
-		double sitelinksAbs = itemDocument.getSiteLinks().size();
-		double sitelinksRel = sitelinksAbs / distinctSitelinks;
-		indexEntity.statistics.put("sitelinksAbs", sitelinksAbs);
-		indexEntity.statistics.put("sitelinksRel", sitelinksRel);
-
-		// Write in JSON file
-		try {
-			jsonGen.writeObject(indexEntity);
-			jsonGen.writeRaw(",\n");
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-
-		// Print object in JSON style
 		System.out.println(indexEntity.toString());
 
 		// Update and print progress
@@ -156,6 +126,114 @@ public class IndexGenerator implements EntityDocumentProcessor {
 	}
 
 	public void processPropertyDocument(PropertyDocument propertyDocument) {
+	}
+
+	/**
+	 * Writes data to JSON Index file.
+	 * 
+	 * @param indexEntity
+	 */
+	private void writeToIndex(IndexEntity indexEntity) {
+		try {
+			jsonGen.writeObject(indexEntity);
+			jsonGen.writeRaw(",\n");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @param itemDocument
+	 * @return
+	 */
+	private IndexEntity retrieveIndexEntity(ItemDocument itemDocument) {
+		IndexEntity indexEntity = new IndexEntity();
+
+		// ID
+		indexEntity.id = itemDocument.getItemId().getId();
+
+		// Surface forms
+		indexEntity.surfaceForms = retrieveSurfaceForms(itemDocument);
+
+		// Statistics
+		indexEntity.statistics = retrieveStatistics(itemDocument);
+
+		return indexEntity;
+	}
+
+	/**
+	 * Given an item document it calculates the statistics of the item.
+	 * 
+	 * @param itemDocument
+	 *            The item which statistics should be calculated.
+	 * @return The statistics for the item.
+	 */
+	private HashMap<String, Double> retrieveStatistics(ItemDocument itemDocument) {
+		HashMap<String, Double> statistics = new HashMap<String, Double>();
+
+		double sitelinksAbs = itemDocument.getSiteLinks().size();
+		double sitelinksRel = sitelinksAbs / distinctSitelinks;
+		statistics.put("sitelinksAbs", sitelinksAbs);
+		statistics.put("sitelinksRel", sitelinksRel);
+
+		return statistics;
+	}
+
+	/**
+	 * Given an item document it retrieves all surface forms of the item for all
+	 * available languages grouped by language.
+	 * 
+	 * @param itemDocument
+	 *            The item which surface forms should be retrieved.
+	 * @return All surface forms of the item grouped by language.
+	 */
+	private HashMap<String, List<String>> retrieveSurfaceForms(ItemDocument itemDocument) {
+		HashMap<String, List<String>> surfaceForms = new HashMap<String, List<String>>();
+		int surfaceFormsCount = 0;
+
+		// Get for all available languages
+		Set<String> languages = itemDocument.getLabels().keySet();
+		for (String language : languages) {
+			List<String> surfaceFormsForLanguage = new ArrayList<String>();
+
+			// Label
+			String label = itemDocument.findLabel(language);
+			if (label != null) {
+				surfaceFormsForLanguage.add(label);
+				surfaceFormsCount++;
+			}
+
+			// Aliases
+			List<MonolingualTextValue> aliases = itemDocument.getAliases().get(language);
+			if (aliases != null) {
+				for (MonolingualTextValue alias : aliases) {
+					surfaceFormsForLanguage.add(alias.getText());
+					surfaceFormsCount++;
+				}
+			}
+
+			surfaceForms.put(language, surfaceFormsForLanguage);
+		}
+
+		// TODO
+		System.out.println(surfaceFormsCount);
+
+		return surfaceForms;
+	}
+
+	public void processItemDocumentById(String itemId) {
+		WikibaseDataFetcher wbdf = WikibaseDataFetcher.getWikidataDataFetcher();
+		try {
+			EntityDocument entityDocument = wbdf.getEntityDocument(itemId);
+			if (entityDocument instanceof ItemDocument) {
+				IndexEntity indexEntity = retrieveIndexEntity((ItemDocument) entityDocument);
+				writeToIndex(indexEntity);
+				System.out.println(indexEntity.toString());
+			}
+		} catch (MediaWikiApiErrorException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
